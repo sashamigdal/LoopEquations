47789f4216320181b030304c5234e872d4d845cb
sashamigdal <sasha.migdal@gmail.com>
2023-06-14 00:41:17 +0400

    replaced the set above dioag method by using a parallel loop by rows, witrh vector setting to zero allk to the right of diag

--- SDE.py
+++ SDE.py
@@ -7,27 +7,31 @@ import multiprocessing as mp
 from parallel import parallel_map, ConstSharedArray, WritableSharedArray
 from plot import Plot, PlotTimed, MakeDir, XYPlot
 from Timer import MTimer as Timer
+
 MakeDir("plots")
 
-global BelowDiagTable
+global NonBelowDiagTable
+
 
 # no changes, git test2
-def MakeBelowEqIdsTable(N):
-    global BelowDiagTable
-    BelowDiagTable = []
+def MakeNonAboveIdsTable(N):
+    global NonBelowDiagTable
+    lst = []
+    NonBelowDiagTable = [[]]
     for n in range(N):
-        BelowDiagTable += [[k,n] for k in range(n+1)]
+        lst += [[k, n] for k in range(n + 1)]
     pass
+
+
 def GetPairs(n):
-    N = int((n*(n+1))/2)
-    return BelowDiagTable[:N]
+    N = int((n * (n + 1)) / 2)
+    return NonBelowDiagTable[:N]
 
 
-def test_BelowDiag():
-    MakeBelowEqIdsTable(6)
-    test =GetPairs(4)
+def test_KeepAboveDiag():
+    MakeNonAboveIdsTable(6)
+    test = GetPairs(5)
     test
-
 
 
 def ff(k, M):
@@ -49,15 +53,17 @@ def testDot3():
 
 
 def gamma_formula(u):
-    assert u.ndim ==1
+    assert u.ndim == 1
     d = u.shape[0]
     V = np.vstack([u.real, u.imag]).reshape(2, d)
     Q = pinvh(V.T.dot(V))
-    V1 = np.dot(Q, V.T)# (d,2)
-    return V1[:,1] + V1[:,0] * 1j
+    V1 = np.dot(Q, V.T)  # (d,2)
+    return V1[:, 1] + V1[:, 0] * 1j
+
 
 def VecGamma(U, Ans):
     Ans[:] = np.vstack([gamma_formula(u) for u in U])
+
 
 def HodgeDual(v):
     return np.array([
@@ -79,6 +85,7 @@ def test_LevyCivita():
     test = c - c1
     test
 
+
 def dot_shape(a, b):
     x = np.zeros_like(a)
     y = np.zeros_like(b)
@@ -99,19 +106,24 @@ def VecKron22(A, B, C):
     assert B.ndim == 2
     C[:] = A[:, :, None] * B[:, None, :]
 
-def parKron22(A,B,C):
+
+def parKron22(A, B, C):
     assert B.shape == A.shape
     assert A.ndim == 2
-    N,d = A.shape
-    assert C.shape == (N,d,d)
+    N, d = A.shape
+    assert C.shape == (N, d, d)
     CS = WritableSharedArray(C)
     AS = ConstSharedArray(A)
     BS = ConstSharedArray(B)
+
     def func(i):
-        CS[i] = np.kron(AS[i,:],BS[i,:]).reshape(d,d)
-    parallel_map(func,[[i] for i in range(N)],mp.cpu_count())
+        CS[i] = np.kron(AS[i, :], BS[i, :]).reshape(d, d)
+
+    parallel_map(func, [[i] for i in range(N)], mp.cpu_count())
     C[:] = CS[:]
     pass
+
+
 def VecDot(a, b):
     M = a.shape[0]
     assert (M == b.shape[0])
@@ -120,27 +132,53 @@ def VecDot(a, b):
     MST = (M, *ST)
     return x.reshape(MST)
 
-def SetDiag(X,f):
+
+def SetDiag(X, f):
     N = len(f)
-    assert X.shape[:2] == (N,N)
+    assert X.shape[:2] == (N, N)
     XS = WritableSharedArray(X)
     fS = ConstSharedArray(f)
+
     def func(k):
         XS[k, k] = fS[k]
-    parallel_map(func,[[k] for k in range(N)],mp.cpu_count())
 
+    parallel_map(func, [[k] for k in range(N)], mp.cpu_count())
 
 def ZeroBelowEqDiag(X):
     N = len(X)
     assert X.shape[:2] == (N, N)
     XS = WritableSharedArray(X)
 
-    def func(*key):
-        XS.clear(key)
-    parallel_map(func,GetPairs(N),mp.cpu_count())
+    def func(i,j):
+        XS.clear((i,j))
+    cores = mp.cpu_count()
+    parallel_map(func, [(k,slice(k,N)) for k in range(N)], 0)
     X[:] = XS[:]
     pass
+def PairsZeroBelowEqDiag(X):
+    N = len(X)
+    assert X.shape[:2] == (N, N)
+    XS = WritableSharedArray(X)
+
+    def func(*key):
+        XS.clear(key)
+
+    parallel_map(func, GetPairs(N), mp.cpu_count())
+    X[:] = XS[:]
+    pass
+
+
+
+
+
+
+
+
+
+
 def testVecOps():
+    MakeNonAboveIdsTable(6)
+
     mp.set_start_method('fork')
     a = np.arange(12).reshape(4, 3)
     b = np.arange(10, 22).reshape(4, 3)
@@ -149,6 +187,10 @@ def testVecOps():
     d = VecDot(a, b)
     assert d.ndim == 1
     assert d.shape[0] == 4
+    X1 = np.arange(36).reshape(3, 3, 2, 2)
+    X2 = X1.copy()
+    ZeroBelowEqDiag(X1)
+    assert (X1 == X2).all
 
 
 class SDEProcess():
@@ -158,7 +200,7 @@ class SDEProcess():
         pass
 
     def Matrix(self, F_flattened):
-        F = F_flattened.reshape(M,3)
+        F = F_flattened.reshape(M, 3)
         q = np.roll(F, 1, axis=0) - F
         # qd0 = np.array([HodgeDual(v) for v in q])
         qd = C33[0]
@@ -166,28 +208,28 @@ class SDEProcess():
         TMP = C33[1]
         VecKron22(F, q, TMP)
         G[:] = np.trace(TMP, axis1=1, axis2=2)
-        G[:] = 2 * G - 1j           #OK, checked  with the paper
+        G[:] = 2 * G - 1j  # OK, checked  with the paper
         U = C[1]
         U[:] = np.trace(TMP.dot(E3), axis1=1, axis2=2)
-        U[:] = G.reshape(M, 1) * U               #OK, checked  with the paper
+        U[:] = G.reshape(M, 1) * U  # OK, checked  with the paper
         # for k in range(M): U[k] = -G[k] * F[k].dot(qd[k])
         gamma = C[2]
-        VecGamma(U, gamma)        #OK, fixed bug  with the paper
+        VecGamma(U, gamma)  # OK, fixed bug  with the paper
         V = C[3]
-        V[:] = G.reshape(M, 1) * q - 2 * F        #OK, fixed bug  with the paper
+        V[:] = G.reshape(M, 1) * q - 2 * F  # OK, fixed bug  with the paper
         gammaXV = C33[1]
-        VecKron22(gamma, V, gammaXV)  #OK, fixed bug  with the paper
+        VecKron22(gamma, V, gammaXV)  # OK, fixed bug  with the paper
         LL[:] = -gammaXV.reshape(M3, 3).dot(qd.reshape(M3, 3).T).reshape(M, 3, M, 3).transpose((0, 2, 1, 3))
-        #LL = gamma_k X Vk dot hat q
+        # LL = gamma_k X Vk dot hat q
         ZeroBelowEqDiag(LL)
         XX = RR[0]
-        XX[:] = LL.real        # Mk>l  = Re( gamma_k X Vk dot hat q)      # Mk<l =0
+        XX[:] = LL.real  # Mk>l  = Re( gamma_k X Vk dot hat q)      # Mk<l =0
         gammaXU = C33[4]
-        VecKron22(gamma, U, gammaXU)      #OK, checked  with the paper
-        SetDiag(XX,gammaXU.real)        # Mkk  = Re( gamma_k X U_k)
+        VecKron22(gamma, U, gammaXU)  # OK, checked  with the paper
+        SetDiag(XX, gammaXU.real)  # Mkk  = Re( gamma_k X U_k)
         XX = XX.transpose((0, 2, 1, 3)).reshape(M3, M3)
         MM = RR[1].transpose((0, 2, 1, 3)).reshape(M3, M3)
-        MM[:] = XX        #eq 2.135b wihout last term
+        MM[:] = XX  # eq 2.135b wihout last term
         LLI = RR[2].transpose((0, 2, 1, 3)).reshape(M3, M3)
         LLI[:] = -LL.imag.transpose((0, 2, 1, 3)).reshape(M3, M3)
         TMP = RR[3].reshape(M3, M3)
@@ -197,12 +239,12 @@ class SDEProcess():
             MM[:] = TMP
         pass
         TMP = C33[3].reshape(M3, 3)
-        Gamma= C33[4].reshape(M3, 3)
-        Gamma[:] = gammaXV.reshape(M3,3)
+        Gamma = C33[4].reshape(M3, 3)
+        Gamma[:] = gammaXV.reshape(M3, 3)
         LLI *= -1
         for _ in range(M - 1):
             np.dot(LLI, np.conjugate(Gamma), TMP)
-            TMP[:] +=gammaXV.reshape(M3,3)
+            TMP[:] += gammaXV.reshape(M3, 3)
             Gamma[:] = TMP
         PP = Gamma.real
         QQ = -Gamma.imag
@@ -218,20 +260,20 @@ class SDEProcess():
         Q = C33[5].transpose(1, 0, 2).reshape(3, M3)
         Q[:] = qd.transpose(1, 0, 2).reshape(3, M3)
         R = C33[6].transpose(1, 0, 2).reshape(3, M3)
-        np.dot(Q,MM,R)
-        R[:] =  1j * Q - R
+        np.dot(Q, MM, R)
+        R[:] = 1j * Q - R
         Lambda = X.dot(R)
-        #HERE I STOPPED
+        # HERE I STOPPED
         Theta = np.conjugate(NS).dot(R).real
         cc = Theta.dot(Theta.T)
-        eig=np.linalg.eigh(cc)
+        eig = np.linalg.eigh(cc)
         cci = pinvh(cc)
         Y = cci.dot(Theta)
         Lambda -= Lambda.dot(Theta.T).dot(Y)
         # test =Lambda.dot(Theta.T)
         # test
         GL = CC[4].reshape(M3, M3)
-        np.dot(Gamma,Lambda,GL)
+        np.dot(Gamma, Lambda, GL)
         MM += GL.real
         TMP1 = RR[3].reshape(M3, M3)
         TMP1[:] = MM - MM.dot(Theta.T).dot(Y)
@@ -242,7 +284,7 @@ class SDEProcess():
             TT[k + 1] += TT[k]
         return TT.transpose((0, 2, 1, 3)).reshape(M3, M3)
 
-    def ItoProcess(self,F0, T, num_steps, chunk):
+    def ItoProcess(self, F0, T, num_steps, chunk):
         tspan = np.linspace(0.0, T, num_steps)
 
         def f(x, t):
@@ -251,17 +293,17 @@ class SDEProcess():
         def g(F, t):
             return self.Matrix(F)
 
-        with Timer("ItoProcess with N=" +str(M)):
+        with Timer("ItoProcess with N=" + str(M)):
             result = ConstSharedArray(np.array(sdeint.itoEuler(f, g, F0, tspan)))
 
-
         def Mean(*key):
-            return np.mean(result[key[0]:key[-1]+1])
+            return np.mean(result[key[0]:key[-1] + 1])
 
         cores = mp.cpu_count()
-        ranges = np.array(range(len(result))).reshape(-1,chunk)
+        ranges = np.array(range(len(result))).reshape(-1, chunk)
         means = np.array(parallel_map(Mean, ranges[1:], cores))
-        XYPlot([means.real, means.imag], plotpath=os.path.join("plots", "test_ito_euler.png"),scatter=False, title='Ito')
+        XYPlot([means.real, means.imag], plotpath=os.path.join("plots", "test_ito_euler.png"), scatter=False,
+               title='Ito')
 
 
 ############### tests
@@ -269,7 +311,6 @@ def testSDE():
     global M, M3, LL, C33, R, C, RR, CC, G
     mp.set_start_method('fork')
     M = 100
-    MakeBelowEqIdsTable(M)
     M3 = M * 3
     F0 = np.array([ff(k, M) for k in range(M)], complex).reshape(M3)
     R = [np.zeros((M, 3), float) for _ in range(6)]
@@ -281,8 +322,8 @@ def testSDE():
     G = np.zeros((M,), complex)
 
     SD = SDEProcess()
-    B =SD.Matrix(F0)
-    SD.ItoProcess(F0,0.5,200,20)
+    B = SD.Matrix(F0)
+    SD.ItoProcess(F0, 0.5, 200, 20)
 
 
 def testFF():
