1de0763a80ede164661269166350c7cbd2496ef3
Alexander Migdal <am10485@nyu.edu>
2023-07-21 15:57:15 -0400

    fixed conflicts to use the hpc version

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -4,21 +4,8 @@
 #SBATCH --mail-user=am10485@nyu.edu
 #SBATCH --cpus-per-task=32
 #SBATCH --output=/scratch/am10485/slurm-logs/slurm-%A_%a.out
-#SBATCH --time=7-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --mem=100GB  
-
-#echo ${SLURM_ARRAY_JOB_ID}
-
-#/tmpdata
-#Removing old slurm files
-#find slurm/. -type f ! -name "slurm-${SLURM_ARRAY_JOB_ID}*" -exec rm {} +
-
-#activate conda environment
-#module load anaconda3/2020.07
-#source /share/apps/NYUAD/miniconda/3-4.11.0/bin/activate
-#activate
-#CPU=48
-#NODE=$SLURM_ARRAY_TASK_ID
 
 NODE=0
 CPU=${SLURM_CPUS_PER_TASK}
--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Export Parameters
-export M=10000000
-export T=10000
+export M=50000000
+export T=1000000
 
-jobid=$(sbatch --array=1-100 --parsable run.sh)
+jobid=$(sbatch --array=1-1000 --parsable run.sh)
 
 sbatch -d afterok:$jobid plot.sh
