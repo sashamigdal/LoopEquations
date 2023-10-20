78266e0c386901eeae5543901c54460ce2a9f874
Max Bulatov <22968713+bulmax@users.noreply.github.com>
2023-10-20 08:58:31 -0400

    [fix] SamplesSorted build in CMake

--- CPP/src/SamplesSorter.cpp
+++ CPP/src/SamplesSorter.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
 #include <iostream>
 #include <fstream>
-#include <filesystem>
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem;
 
 #pragma pack(push, 1)
 struct Sample {
@@ -14,14 +16,14 @@ int main( int argc, const char* argv[] ) {
         std::cout << "Usage: " << argv[0] << " <file path> <number of pieces>" << std::endl;
         return 1;
     }
-    std::filesystem::path filepath( argv[1] );
-    std::filesystem::path dirpath = filepath.parent_path();
+    fs::path filepath( argv[1] );
+    fs::path dirpath = filepath.parent_path();
     std::ifstream fIn( filepath, std::ios::binary );
     if ( !fIn ) {
         std::cerr << "Couldn't open file " << filepath << std::endl;
         return 1;
     }
-    const auto filesize = std::filesystem::file_size(filepath);
+    const auto filesize = fs::file_size(filepath);
     const auto nsamples = filesize / sizeof(double) / 3 / 32;
     const size_t nPieces = std::atoi( argv[2] );
     if ( filesize % (sizeof(double) * 3 * 32 * nPieces) != 0 ) {
@@ -32,7 +34,7 @@ int main( int argc, const char* argv[] ) {
     fIn.read( (char*) &samples[0], filesize );
     std::sort( std::begin(samples), std::end(samples), []( const Sample& lhs, const Sample& rhs ) {return lhs.field[0][0] < rhs.field[0][0]; } );
     for ( size_t i = 0; i != nPieces; i++ ) {
-        std::filesystem::path outfilepath = dirpath / ("FDStatsPiece." + std::to_string(i) + ".np");
+        fs::path outfilepath = dirpath / ("FDStatsPiece." + std::to_string(i) + ".np");
         std::ofstream fOut( outfilepath, std::ios::binary );
         size_t beg = nsamples * i / nPieces;
         fOut.write( (char*) &samples[beg], filesize / nPieces );
