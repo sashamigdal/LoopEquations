87b6a0653110bf325c88af9e8a421c3cc9f84d1e
Max Bulatov <22968713+bulmax@users.noreply.github.com>
2024-02-21 05:38:48 +0400

    [scripts] Updated scripts from the cluster

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=am10485@nyu.edu
 #SBATCH --cpus-per-task=32
-#SBATCH --output=/scratch/am10485/slurm-logs/slurm-%A_%a.out
+#SBATCH --output=/scratch/am10485/slurm-logs/plot-%A_%a.out
 #SBATCH --time=00:30:00
 #SBATCH --mem=100GB
 
@@ -15,4 +15,4 @@ source /share/apps/NYUAD/miniconda/3-4.11.0/bin/activate py39
 echo "Starting collecting script . . . "
 # python -u VorticityCorrelation.py -M ${M} -EG E -C 0 --run 2 --compute GPU
 cd CPP/cmake-build-release
-./SamplesBinner ${PROJECT_DIR}/plots/VorticityCorr.${M}.GPU.${RUN_ID} 20
+./SamplesBinner ${PROJECT_DIR}/plots/VorticityCorr.${M}.GPU.${RUN_ID} 24
--- scripts/submit_gpu.sh
+++ scripts/submit_gpu.sh
@@ -3,21 +3,21 @@
 # 2x4, 6x2, 10x1 (jobs x GPUs) are OK.
 
 #Export Parameters
-NJOBS=10
+NJOBS=480
 export NCPUS=8
-NGPUS=2
+NGPUS=1
 
 # Memory size, in GB:
-export MEM_SIZE=16
-export M=250000000
+export MEM_SIZE=20
+export M=2000000
 export T=524288
-export RUN_ID=1
+export RUN_ID=0
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PROJECT_DIR="$(dirname "${SCRIPT_DIR}")"
 
-jobid=$(sbatch -p nvidia -q nvidia-xxl --array=1-${NJOBS} --cpus-per-task=${NCPUS} --mem=${MEM_SIZE}GB --gres=gpu:a100:${NGPUS} -t 3-00:00:00 --parsable run_gpu.sh)
+jobid=$(sbatch -p nvidia -q nvidia-xxl --array=1-${NJOBS} --cpus-per-task=${NCPUS} --mem=${MEM_SIZE}GB --gres=gpu:a100:${NGPUS} -t 10-00:00:00 --parsable run_gpu.sh)
 echo Submitted GPU jobs ${jobid}
 
-jobid2=$(sbatch --dependency=afterok:${jobid} --parsable plot.sh)
-echo Submitted binning job ${jobid2}
+#jobid2=$(sbatch --dependency=afterok:${jobid} --parsable plot.sh)
+#echo Submitted binning job ${jobid2}
