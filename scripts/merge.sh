9cdcab6cc978d0650ea5bee712fe9a5c7edbda01
Max Bulatov <22968713+bulmax@users.noreply.github.com>
2024-02-22 15:54:17 +0500

    [add][SamplesBinner] Merging

---
+++ scripts/merge.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=am10485@nyu.edu
+#SBATCH --cpus-per-task=1
+#SBATCH --output=/scratch/am10485/slurm-logs/index-%A_%a.out
+#SBATCH --time=00:30:00
+#SBATCH --mem=2GB
+
+echo PROJECT_DIR=${PROJECT_DIR}
+cd ${PROJECT_DIR}
+
+echo "Starting indexing . . . "
+cd CPP/cmake-build-release
+./SamplesBinner --merge ${PROJECT_DIR}/plots/VorticityCorr.${M}.GPU.${RUN_ID}/Fdata.E.${T}.$(($SLURM_ARRAY_TASK_ID*2)).sorted.np
